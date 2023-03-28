3a5530ad83394d709abe4e624b43e06fbb28c44b
Rasyid Rafi <rasyid.rafi@yahoo.com>
2023-03-28 19:00:56 +0700

    react-paper barebone

---
+++ config-overrides.js
@@ -0,0 +1,33 @@
+module.exports = function override(config, env) {
+  config.module.rules.push(
+    // {
+    //   test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+    //   loader: 'file-loader',
+    // },
+    {
+      test: /\.js$/,
+      exclude: /node_modules[/\\](?!react-native-vector-icons)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          // Disable reading babel configuration
+          babelrc: false,
+          configFile: false,
+
+          // The configuration for compilation
+          presets: [
+            ["@babel/preset-env", { useBuiltIns: "usage", "corejs": "3" }],
+            "@babel/preset-react",
+            "@babel/preset-flow",
+            "@babel/preset-typescript"
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-object-rest-spread"
+          ]
+        }
+      }
+    });
+
+  return config;
+};
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <PaperProvider>
+    <style type="text/css">
+      {`
+        @font-face {
+          font-family: 'MaterialCommunityIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
+        }
+      `}
+    </style>
     <App />
-  </React.StrictMode>
+  </PaperProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
